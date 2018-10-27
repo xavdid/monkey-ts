cf601dc87c0b199ad1d39faabe796b2cfa4657c9
David Brownman <beamneocube@gmail.com>
2018-10-27 18:24:41 -0400

    repl runs!

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export const sum = (a: number, b: number) => {
-  return a + b
+#!/usr/bin/env node
+
+import repl from './repl'
+
+function main() {
+  console.log('Hello! This is the Monkey programming language')
+  console.log('Feel free to type in commands')
+  repl()
 }
+
+main()
---
+++ src/repl.ts
@@ -0,0 +1,23 @@
+import * as repl from 'repl'
+import { Lexer } from './lexer'
+import { TOKENS } from './token'
+
+function lineToTokens(
+  line: string,
+  context: any, // who knows what these are for
+  filename: any, // who knows what these are for
+  callback: (err: Error | null, content?: any) => void
+) {
+  const l = new Lexer(line)
+  for (let tok = l.nextToken(); tok.type !== TOKENS.EOF; tok = l.nextToken()) {
+    console.log(tok)
+  }
+  callback(null)
+}
+
+export default function() {
+  repl.start({
+    prompt: '>> ',
+    eval: lineToTokens
+  })
+}
